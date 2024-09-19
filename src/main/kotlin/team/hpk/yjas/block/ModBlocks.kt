0cb1d23864e5b0a9b40888d3f96edf639e929d7f
Mike Tang <eartzh6563@gmail.com>
2024-09-19 14:02:17 +0800

    refactor: Block Settings

--- src/main/kotlin/team/hpk/yjas/block/ModBlocks.kt
+++ src/main/kotlin/team/hpk/yjas/block/ModBlocks.kt
@@ -21,8 +21,8 @@ package team.hpk.yjas.block
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
-import net.minecraft.registry.Registries
-import net.minecraft.registry.Registry
+import net.minecraft.block.Material
+import net.minecraft.util.registry.Registry
 import team.hpk.yjas.Utils.getIdentifier
 
 
@@ -30,21 +30,21 @@ object ModBlocks {
 
     val SILVER_BLOCK: Block = Block(
         FabricBlockSettings
-            .create()
+            .of(Material.METAL)
             .strength(4.0f)
             .requiresTool()
     )
 
     val SILVER_ORE: Block = Block(
         FabricBlockSettings
-            .create()
+            .of(Material.STONE)
             .strength(3.0f)
             .requiresTool()
     )
 
     val DEEPSLATE_SILVER_ORE: Block = Block(
         FabricBlockSettings
-            .create()
+            .of(Material.STONE)
             .strength(4.5f)
             .requiresTool()
     )
@@ -52,19 +52,19 @@ object ModBlocks {
 
     fun register() {
         Registry.register(
-            Registries.BLOCK,
+            Registry.BLOCK,
             getIdentifier("silver_block"),
             SILVER_BLOCK
         )
 
         Registry.register(
-            Registries.BLOCK,
+            Registry.BLOCK,
             getIdentifier("silver_ore"),
             SILVER_ORE
         )
 
         Registry.register(
-            Registries.BLOCK,
+            Registry.BLOCK,
             getIdentifier("deepslate_silver_ore"),
             DEEPSLATE_SILVER_ORE
         )
