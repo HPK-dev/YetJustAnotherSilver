dbfeef26ecac1829f0d69e9deebae2a638f99e56
Mike Tang <eartzh6563@gmail.com>
2024-09-19 21:15:29 +0800

    refactor(datagen): Recipe

--- src/main/kotlin/team/hpk/yjas/datagen/Recipe.kt
+++ src/main/kotlin/team/hpk/yjas/datagen/Recipe.kt
@@ -19,14 +19,13 @@
 
 package team.hpk.yjas.datagen
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.data.server.recipe.RecipeJsonProvider
-import net.minecraft.recipe.book.RecipeCategory
 import team.hpk.yjas.item.ModItems
 import java.util.function.Consumer
 
-class Recipe(output: FabricDataOutput) : FabricRecipeProvider(output) {
+class Recipe(output: FabricDataGenerator) : FabricRecipeProvider(output) {
 
     companion object {
         private val SILVER_MELTABLE = listOf(
@@ -36,29 +35,29 @@ class Recipe(output: FabricDataOutput) : FabricRecipeProvider(output) {
         )
     }
 
-    override fun generate(exporter: Consumer<RecipeJsonProvider>) {
+    override fun generateRecipes(exporter: Consumer<RecipeJsonProvider>?) {
         offerSmelting(
             exporter, SILVER_MELTABLE,
-            RecipeCategory.MISC, ModItems.SILVER_INGOT,
+             ModItems.SILVER_INGOT,
             1.0f, 200, "silver"
         )
 
         offerBlasting(
-            exporter, SILVER_MELTABLE, RecipeCategory.MISC, ModItems.SILVER_INGOT,
+            exporter, SILVER_MELTABLE, ModItems.SILVER_INGOT,
             1.0f, 200, "silver"
         )
 
         offerReversibleCompactingRecipesWithCompactingRecipeGroup(
             exporter,
-            RecipeCategory.MISC, ModItems.SILVER_INGOT,
-            RecipeCategory.BUILDING_BLOCKS, ModItems.SILVER_BLOCK,
+             ModItems.SILVER_INGOT,
+             ModItems.SILVER_BLOCK,
             "silver_block_from_ingot","silver"
         )
 
         offerReversibleCompactingRecipesWithCompactingRecipeGroup(
             exporter,
-            RecipeCategory.MISC, ModItems.SILVER_NUGGET,
-            RecipeCategory.MISC, ModItems.SILVER_INGOT,
+             ModItems.SILVER_NUGGET,
+             ModItems.SILVER_INGOT,
             "silver_ingot_from_nugget","silver"
         )
 
