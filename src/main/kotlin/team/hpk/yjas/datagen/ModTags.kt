8eb3014f85c95c7b7e1c1d56331f057cb90dd289
Mike Tang <eartzh6563@gmail.com>
2025-07-30 23:20:06 +0800

    fix: incorrect or missing tags.

--- src/main/kotlin/team/hpk/yjas/datagen/ModTags.kt
+++ src/main/kotlin/team/hpk/yjas/datagen/ModTags.kt
@@ -32,17 +32,23 @@ object ModTags {
             TagKey.of(RegistryKeys.ITEM, Identifier.of(id))
 
         val ORES = key("c:ores")
+        val ORES_SILVER = key("c:ores/silver")
         val SILVER_ORES = key("c:silver_ores")
         val SILVER_BLOCKS = key("c:silver_blocks")
-        val RAW_ORES = key("c:raw_ores")
+        val RAW_MATERIALS = key("c:raw_materials")
+        val RAW_MATERIALS_SILVER = key("c:raw_materials/silver")
         val RAW_SILVER = key("c:raw_silver")
         val RAW_SILVER_ORES= key("c:raw_silver_ores")
         val INGOTS = key("c:ingots")
+        val INGOTS_SILVER = key("c:ingots/silver")
         val NUGGETS = key("c:nuggets")
+        val NUGGETS_SILVER = key("c:nuggets/silver")
+        val STORAGE_BLOCKS = key("c:storage_blocks")
+        val STORAGE_BLOCKS_SILVER = key("c:storage_blocks/silver")
         val SILVER_INGOTS = key("c:silver_ingots")
         val SILVER_NUGGETS = key("c:silver_nuggets")
-        val ORES_IN_GROUND_STONE = key("c:ore_in_ground/stone")
-        val ORES_IN_GROUND_DEEPSLATE = key("c:ore_in_ground/deepslate")
+        val ORES_IN_GROUND_STONE = key("c:ores_in_ground/stone")
+        val ORES_IN_GROUND_DEEPSLATE = key("c:ores_in_ground/deepslate")
 
     }
 
@@ -54,7 +60,7 @@ object ModTags {
         val BLOCKS = key("c:blocks")
         val SILVER_ORES = key("c:silver_ores")
         val SILVER_BLOCKS = key("c:silver_blocks")
-        val ORES_IN_GROUND_STONE = key("c:ore_in_ground/stone")
-        val ORES_IN_GROUND_DEEPSLATE = key("c:ore_in_ground/deepslate")
+        val ORES_IN_GROUND_STONE = key("c:ores_in_ground/stone")
+        val ORES_IN_GROUND_DEEPSLATE = key("c:ores_in_ground/deepslate")
     }
 }
